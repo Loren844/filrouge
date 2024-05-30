dcd5be8f6000ea1bc58644cc2a65e7a6d2444156
Loren844 <lobaladier@gmail.com>
2024-05-30 09:30:43 +0200

    enlever classes inutiles

--- app/src/main/java/r411/filrouge/PostExecuteActivity.java
+++
@@ -1,8 +0,0 @@
-package r411.filrouge;
-
-import java.util.List;
-
-public interface PostExecuteActivity<T> {
-    void onPostExecute(List<T> itemList);
-    void runOnUiThread(Runnable runable);
-}
